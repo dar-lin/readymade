376c0411a803e3b5871c3dab8a0e220fa49f04eb
dar-lin <christina.holevas@gmail.com>
2020-05-13 16:58:42 -0400

    changed jpg to lowercase

--- javahometest.js
+++ javahometest.js
@@ -1,47 +1,47 @@
 function setNewImageSaturn(){
-    document.getElementById("saturn").src="Art/HOMECOVERS/saturnbackground.JPG"
+    document.getElementById("saturn").src="Art/HOMECOVERS/saturnbackground.jpg"
 }
 function setOldImageSaturn(){
     document.getElementById("saturn").src="COVERS/SATURNcover.jpg"
 }
 
 function setNewImageIsolation(){
-    document.getElementById("isolation").src="Art/HOMECOVERS/isolationbackground.JPG"
+    document.getElementById("isolation").src="Art/HOMECOVERS/isolationbackground.jpg"
 }
 function setOldImageIsolation(){
     document.getElementById("isolation").src="COVERS/BOEcover.jpg"
 }
 
 function setNewImage320(){
-    document.getElementById("320").src="Art/HOMECOVERS/320background.JPG"
+    document.getElementById("320").src="Art/HOMECOVERS/320background.jpg"
 }
 function setOldImage320(){
     document.getElementById("320").src="COVERS/320cover.jpg"
 }
 
 function setNewImageMemo(){
-    document.getElementById("memo").src="COVERS/READYSOONcover.JPG"
+    document.getElementById("memo").src="COVERS/READYSOONcover.jpg"
 }
 function setOldImageMemo(){
     document.getElementById("memo").src="COVERS/MEMOcover.jpg"
 }
 
 function setNewImageIdle(){
-    document.getElementById("idle").src="COVERS/READYSOONcover.JPG"
+    document.getElementById("idle").src="COVERS/READYSOONcover.jpg"
 }
 function setOldImageIdle(){
     document.getElementById("idle").src="COVERS/IDLEcover.jpg"
 }
 
 function setNewImageHead(){
-    document.getElementById("headprop").src="COVERS/READYSOONcover.JPG"
+    document.getElementById("headprop").src="COVERS/READYSOONcover.jpg"
 }
 function setOldImageHead(){
     document.getElementById("headprop").src="COVERS/HEADPROPcover.jpg"
 }
 
 function setNewImageSaturn(){
-    document.getElementById("saturn").src="Art/HOMECOVERS/saturnbackground.JPG"
+    document.getElementById("saturn").src="Art/HOMECOVERS/saturnbackground.jpg"
 }
 function setOldImageSaturn(){
     document.getElementById("saturn").src="COVERS/SATURNcover.jpg"
