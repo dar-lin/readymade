1cea99e8031333e2831948ed6b0813270ef184f8
dar-lin <christina.holevas@gmail.com>
2020-05-13 15:46:59 -0400

    created Java folder

---
+++ JAVA/home.js
@@ -0,0 +1,43 @@
+function setNewImageSaturn(){
+    document.getElementById("saturn").src="../ART/HOMECOVERS/saturn.jpg"  
+}
+
+function setNewImageIsolation(){
+    document.getElementById("isolation").src="../ART/HOMECOVERS/isolation.jpg"
+
+
+
+function setOldImage(){
+    document.getElementById("imageTest").src="finalcovers/Mustafa COVER FINAL.jpg"
+    
+}
+
+function setOldImage1(){
+    document.getElementById("imageTest1").src="finalcovers/Peter COVER FINAL.jpg"
+}
+
+function setOldImage2(){
+    document.getElementById("imageTest2").src="finalcovers/BOE COVER FINAL.jpg"
+}
+function setNewImageMemo(){
+    document.getElementById("imageTestMemo").src="COVERS/ready soon 2.jpg"; 
+}
+function setNewImageIdle(){
+    document.getElementById("imageTestIdle").src="COVERS/ready soon 2.jpg"; 
+}
+function setNewImageHead(){
+    document.getElementById("imageTestHead").src="COVERS/ready soon 2.jpg"; 
+}
+function setOldImageMemo(){
+    document.getElementById("imageTestMemo").src="COVERS/memo cover 2.jpg"
+}
+function setOldImageIdle(){
+    document.getElementById("imageTestIdle").src="COVERS/nicolas cover.jpg"
+}
+function setOldImageHead(){
+    document.getElementById("imageTestHead").src="COVERS/HEADPROP.jpg"
+}
+
+
+const saturn = document.querySelector("img.saturn")
+
