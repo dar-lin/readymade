d31de501465bfc4524fd0d7639736ab04b3f1453
dar-lin <63255903+dar-lin@users.noreply.github.com>
2020-05-13 15:38:15 -0400

    adding home files

---
+++ javahometest.js
@@ -0,0 +1,48 @@
+function setNewImageSaturn(){
+    document.getElementById("saturn").src="ART/HOMECOVERS/saturnbackground.JPG"
+}
+function setOldImageSaturn(){
+    document.getElementById("saturn").src="COVERS/SATURNcover.jpg"
+}
+
+function setNewImageIsolation(){
+    document.getElementById("isolation").src="ART/HOMECOVERS/isolationbackground.JPG"
+}
+function setOldImageIsolation(){
+    document.getElementById("isolation").src="COVERS/BOEcover.jpg"
+}
+
+function setNewImage320(){
+    document.getElementById("320").src="ART/HOMECOVERS/320background.JPG"
+}
+function setOldImage320(){
+    document.getElementById("320").src="COVERS/320cover.jpg"
+}
+
+function setNewImageMemo(){
+    document.getElementById("memo").src="COVERS/READYSOONcover.JPG"
+}
+function setOldImageMemo(){
+    document.getElementById("memo").src="COVERS/MEMOcover.jpg"
+}
+
+function setNewImageIdle(){
+    document.getElementById("idle").src="COVERS/READYSOONcover.JPG"
+}
+function setOldImageIdle(){
+    document.getElementById("idle").src="COVERS/IDLEcover.jpg"
+}
+
+function setNewImageHead(){
+    document.getElementById("headprop").src="COVERS/READYSOONcover.JPG"
+}
+function setOldImageHead(){
+    document.getElementById("headprop").src="COVERS/HEADPROPcover.jpg"
+}
+
+function setNewImageSaturn(){
+    document.getElementById("saturn").src="ART/HOMECOVERS/saturnbackground.JPG"
+}
+function setOldImageSaturn(){
+    document.getElementById("saturn").src="COVERS/SATURNcover.jpg"
+}
