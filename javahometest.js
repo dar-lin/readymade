9ae8c5da8e8d0efaf4530d638b84049215482619
dar-lin <christina.holevas@gmail.com>
2020-05-13 20:59:50 -0400

    adding nico cover on java

--- javahometest.js
+++ javahometest.js
@@ -27,7 +27,7 @@ function setOldImageMemo(){
 }
 
 function setNewImageIdle(){
-    document.getElementById("idle").src="Art/HOMECOVERS/readysoonbackground.jpg"
+    document.getElementById("idle").src="Art/HOMECOVERS/idlebackground.jpg"
 }
 function setOldImageIdle(){
     document.getElementById("idle").src="COVERS/IDLEcover.jpg"
