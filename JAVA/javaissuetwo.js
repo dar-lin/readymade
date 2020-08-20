2e1313a33baa9e3730d5fcbea81b23a6a122a8a3
dar-lin <christina.holevas@gmail.com>
2020-08-20 18:23:24 -0400

    java for issue two homepage

---
+++ JAVA/javaissuetwo.js
@@ -0,0 +1,32 @@
+const slideArea = document.querySelector("div.slides")
+const images = slideArea.querySelectorAll("img")
+const counter = document.querySelector("div.counter")
+const rightLayer = document.querySelector("div.right-layer")
+const leftLayer = document.querySelector("div.left-layer")
+
+let currentSlide = 0
+let z = 1
+
+rightLayer.addEventListener("click", function() {
+    currentSlide = currentSlide + 1
+    z = z + 1
+
+    if (currentSlide > 4) {
+        currentSlide = 0
+    }
+
+    images[currentSlide].style.zIndex = z
+
+    counter.innerHTML = `${currentSlide + 1} / ${images.length}`
+})
+
+leftLayer.addEventListener("click", function (){
+    if (currentSlide < 1){
+      currentSlide = 5
+    }
+    currentSlide = currentSlide - 1
+    z = z + 1
+    images[currentSlide].style.zIndex = z
+    
+    counter.innerHTML = `${currentSlide + 1} / ${images.length}`
+  })
